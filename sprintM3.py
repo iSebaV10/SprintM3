4e590d3095d940cde20ebf45e3d50b73e30ceb44
iSebaV10 <alvaro.valdivia10@gmail.com>
2023-05-31 19:51:04 -0400

    Se realiza Sprint M3

--- sprintM3.py
+++ sprintM3.py
@@ -6,20 +6,20 @@ usuarios = ["Usuario1", "Usuario2", "Usuario3", "Usuario4", "Usuario5", "Usuario
 
 #Funcion para generar password con los criterios señalados
 def generar_pass():
-    letras_mayusculas = string.ascii_uppercase
-    letras_minusculas = string.ascii_lowercase
+    mayusculas = string.ascii_uppercase
+    minusculas = string.ascii_lowercase
     numeros = string.digits
 
-    # Combinamos los caracteres en una lista
-    caracteres = letras_mayusculas + letras_minusculas + numeros
+    # Combinamos los caracteres arrojados anteriormente
+    caracteres = mayusculas + minusculas + numeros
 
-    # Elegimos al menos un carácter de cada tipo
-    contraseña = random.choice(letras_mayusculas) + random.choice(letras_minusculas) + random.choice(numeros)
+    # Elejo al menos 1 caracter de cada tipo de manera aleatoria
+    contraseña = random.choice(mayusculas) + random.choice(minusculas) + random.choice(numeros)
 
-    # Generamos el resto de la contraseña aleatoriamente
+    # Genero el resto de la contraseña de forma aleatoria 
     contraseña += ''.join(random.choice(caracteres) for _ in range(8))
 
-    # Barajamos la contraseña para que no sea predecible
+    # Código para mezclar los caracteres y que no queden fijos ni sea predecible
     contraseña_lista = list(contraseña)
     random.shuffle(contraseña_lista)
     contraseña = ''.join(contraseña_lista)
@@ -30,6 +30,7 @@ def generar_pass():
 def obtener_numero_telefonico(usuario):
     while True:
         telefono = input(f"Ingrese el número telefónico para {usuario}: ")
+        #Verifico que lo que ingresa el usuario son digitos y que el largo sea igual a 8 caracteres
         if telefono.isdigit() and len(telefono) == 8:
             return telefono
         print("El número telefónico debe tener 8 dígitos. Inténtelo nuevamente.")
@@ -39,15 +40,25 @@ cuentas = {}
 
 #Guardo pass y telefono en la lista cuenta
 for usuario in usuarios:
+    #Se llama a la funcion para crear una contraseña
     contraseña = generar_pass()
+    #Se llama a la función para obtener el numero telefonico del usuario
     telefono = obtener_numero_telefonico(usuario)
+    #se añaden ambos a cuenta
     cuenta = {
         "contraseña": contraseña,
         "telefono": telefono
     }
     cuentas[usuario] = cuenta
 
-# Imprimir todas las cuentas y sus detalles
+''' Pruebas
+print(f"lista de usuarios: {usuarios}")
+print(f"Cuenta: {cuenta}")
+print(f"Cuentas: {cuentas}")
+'''
+
+
+# Se imprimen todas las cuentas y sus detalles
 for usuario, detalles in cuentas.items():
     print()
     print(f"Usuario: {usuario}")
