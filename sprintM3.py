611bbee64ff5476959031de8ff0b098f5778c1d4
iSebaV10 <alvaro.valdivia10@gmail.com>
2023-05-30 21:34:46 -0400

    Cambio nombre del archivo

---
+++ sprintM3.py
@@ -0,0 +1,19 @@
+'''
+● Elaborar un programa que recorra una lista con los nombres de 10 de sus futuros usuarios de tu
+aplicación (pueden ser personas, pacientes, organizaciones sociales o instituciones públicas).
+● Mediante una función, a todos los usuarios se les creará una cuenta automáticamente.
+● Asigne una contraseña para cada cuenta. La contraseña debe ser creada con random y debe
+cumplir con los siguientes criterios: mayúsculas, minúsculas y números.
+● Cada cuenta debe guardarse en una nueva variable con su respectiva contraseña.
+● Por cada cuenta debe pedir un número telefónico para contactarse.
+
+● El programa no terminará de preguntar por los números hasta que todas las organizaciones
+tengan un número de contacto asignado.
+● El programa debe verificar que el número telefónico tenga 8 dígitos numéricos.
+● Se debe guardar como un string.
+'''
+
+usuarios = ["Usuario1", "Usuario2", "Usuario3", "Usuario4", "Usuario5", "Usuario6", "Usuario7", "Usuario8", "Usuario9", "Usuario10"]
+
+for usuario in usuarios:
+    print(usuario)
